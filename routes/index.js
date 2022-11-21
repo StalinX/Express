80ad1f0d99fca286bff791fe2dd734dc67896483
StalinX <sprt.prof@gmail.com>
2022-11-20 19:17:59 -0500

    express

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const booksRouter = require('./books.router');
+//const clientsRouter = require('/clients.router');
+const authorsRouter = require('./authors.router');
+const operationRouter = require('./operation.router');
+
+
+const routerApi =(app) =>{
+     app.use('./books', booksRouter);
+     app.use('./authors', authorsRouter);
+     //app.use('./clients/:clientId/books/:bookId', clientsRouter);
+    app.use('./operation', operationRouter);
+}
+
+module.exports = routerApi;
